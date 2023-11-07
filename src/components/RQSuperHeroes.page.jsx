f1f28fa5e054f63b7f5db8d11b8b99a117f584ac
Akingbade Ayotunde <tylerkatsumoto@gmail.com>
2023-11-07 16:22:59 +0100

    calling a query on Click of a button

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -6,19 +6,20 @@ const fetchSuperHeroes = () => {
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error } = useQuery(
+  const { isLoading, data, isError, error, refetch } = useQuery(
     'super-heroes',
     fetchSuperHeroes,
     {
       // after 5 seconds the caache for react-Query [super-heroes] is garbaged i.e no memory storage for it again
       // the default if not set is 5 minutes
-      cacheTime: 5000,
-      // refetch defaults
-      refetchOnMount: true,
-      refetchOnWindowFocus: true,
-      //   polling
-      refetchInterval: 2000,
-      refetchIntervalInBackground: true,
+      //   cacheTime: 5000,
+      // ======REFETCH DEFAULTS=====
+      //   refetchOnMount: true,
+      //   refetchOnWindowFocus: true,
+      //  ======= POLLING=====
+      //   refetchInterval: 2000,
+      //   refetchIntervalInBackground: true,
+      enabled: false,
     }
   );
 
@@ -31,6 +32,7 @@ export const RQSuperHeroesPage = () => {
   }
   return (
     <>
+      <button onClick={refetch}>Fetch Heroes</button>
       <h2>RQ SuperHeroes page</h2>
       {data?.data.map((heroes) => {
         return <div key={heroes.name}>{heroes.name}</div>;
