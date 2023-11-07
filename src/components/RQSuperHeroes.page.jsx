a97ee342bad438c554acc72b7007e81ab716f71e
Akingbade Ayotunde <tylerkatsumoto@gmail.com>
2023-11-07 15:08:52 +0100

    error handling for query

--- src/components/RQSuperHeroes.page.jsx
+++ src/components/RQSuperHeroes.page.jsx
@@ -2,14 +2,21 @@ import { useQuery } from 'react-query';
 import axios from 'axios';
 
 const fetchSuperHeroes = () => {
-  return axios.get('http://localhost:4000/superHeroes');
+  return axios.get('http://localhost:4000/superHeroes2');
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    'super-heroes',
+    fetchSuperHeroes
+  );
 
   if (isLoading) {
     return <h2> ...Is Loading </h2>;
+  }
+
+  if (isError) {
+    return <h2> {error.message} </h2>;
   }
   return (
     <>
